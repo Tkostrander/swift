8754890811a1ca32853f681eb9cf277af3dcd89f
Tkostrander <101438078+Tkostrander@users.noreply.github.com>
2022-12-09 16:39:14 -0700

    Add files via upload

---
+++ aPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let a = 1
+let b = 1
+if a < 4 && b < 4 {
+    print("branch 1")
+}
+else if a < 8 {
+    print("branch 2")
+}
+else if a == 10 {
+    print("branch 3")
+}
+else {
+    print("nothing was true")
+}
+
+
+
